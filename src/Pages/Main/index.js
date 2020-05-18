58709b49567572bddeda798d781751c39aae794c
Felipe Ken <fkenhirano4@gmail.com>
2020-05-18 19:08:00 -0300

    Ajuste na biblioteca do realm e ajustes para inserção do Entries.js

--- src/Pages/Main/index.js
+++ src/Pages/Main/index.js
@@ -3,17 +3,20 @@ import {View, Button, StyleSheet} from 'react-native';
 import BalancePanel from '../../components/BalancePanel';
 import EntrySummary from '../../components/EntrySummary';
 import EntryList from '../../components/EntryList';
+import {saveEntry} from '../../services/Entries';
 
 const Main = ({navigation}) => {
   const saldo = '$2.064,37';
 
+  // () => navigation.navigate('NewEntry')
+  const save = () => {
+    saveEntry();
+  };
+
   return (
     <View style={styles.container}>
-      <BalancePanel currentBalance={saldo}/>
-      <Button
-        title="Adicionar"
-        onPress={() => navigation.navigate('NewEntry')}
-      />
+      <BalancePanel currentBalance={saldo} />
+      <Button title="Adicionar" onPress={save} />
       <EntrySummary />
       <EntryList />
     </View>
--- src/services/Entries.js
+++ src/services/Entries.js
@@ -1,18 +1,25 @@
 import {getRealm} from './Realm';
+import {Alert} from 'react-native';
 
 export const saveEntry = async () => {
-  const realm = getRealm();
+  const realm = await getRealm();
   let data = {};
-  realm.write(() => {
-    data = {
-      id: 'ABC', //abraubsua - codigo aleatorio
-      amount: '12.4',
-      entryAt: new Date(),
-      isInit: false,
-    };
 
-    realm.create('Entry', data, true);
-  });
-  console.log(data);
+  try {
+    realm.write(() => {
+      data = {
+        id: 'ABC', //abraubsua - codigo aleatorio
+        amount: 12.4,
+        entryAt: new Date(),
+        isInit: false,
+      };
+
+      realm.create('Entry', data, true);
+    });
+    console.log(data);
+  } catch (error) {
+    console.error('saveEntry :: error on save object :', JSON.stringify(data));
+    Alert.alert('Erro ao salvar os dados de lançamento');
+  }
   return data;
 };
